b3f9645c4a73e0bd4498a3e46ab2c9130e0767f6
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-05-02 11:00:11 +0700

    Fis users models

--- src/models/Users.js
+++ src/models/Users.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose')
 const data_schema = mongoose.Schema({
   username: String,
   google_id: String,
+  email: String,
   complete_data: { type: Boolean, default: false },
+  data: {
+    gender: String,
+    phone: Number,
+    address: String,
+    birth: Date,
+  }
 })
 
 module.exports = mongoose.model('Users', data_schema)
