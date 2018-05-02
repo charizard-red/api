b85d691dc4dbd762990df9be6239f2097f4a742b
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-05-02 10:43:12 +0700

    Add complete data to Users model

--- src/models/Users.js
+++ src/models/Users.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const data_schema = mongoose.Schema({
   username: String,
-  google_id: String
+  google_id: String,
+  complete_data: { type: Boolean, default: false },
 })
 
 module.exports = mongoose.model('Users', data_schema)
