7f6fd993e90b05365748a693d20306ef79cd65ff
M Haidar Hanif <git@mhaidarhanif.com>
2018-05-03 17:43:26 +0700

    Use MONGODB_URL in app index

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const express = require('express')
-const passport = require('passport')
-const express_fileupload = require('express-fileupload')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require("express");
+const passport = require("passport");
+const express_fileupload = require("express-fileupload");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config()
-mongoose.connect('mongodb://localhost/doctor')
-require('./src/config/passport')
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost/doctor";
 
-const app = express()
+require("dotenv").config();
+mongoose.connect(MONGODB_URL);
+require("./src/config/passport");
 
-app.use(cors())
-app.use(passport.initialize())
-app.use(express_fileupload())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+const app = express();
 
-app.use('/auth', require('./src/routes/auth'))
-app.use('/clinic', require('./src/routes/clinic'))
-app.use('/orders', require('./src/routes/orders'))
-app.use('/doctors', require('./src/routes/doctor'))
+app.use(cors());
+app.use(passport.initialize());
+app.use(express_fileupload());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.listen(process.env.PORT || 8000, () => console.log('listening on port 8000'))
+app.use("/auth", require("./src/routes/auth"));
+app.use("/clinic", require("./src/routes/clinic"));
+app.use("/orders", require("./src/routes/orders"));
+app.use("/doctors", require("./src/routes/doctor"));
+
+app.listen(process.env.PORT || 8000, () =>
+  console.log("listening on port 8000")
+);
