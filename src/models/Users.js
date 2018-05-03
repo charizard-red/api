1553e4413696c6afe4994a4f908ee3e159ffd515
Dei Gratia R R <rdeigratia95@gmail.com>
2018-05-03 18:30:47 +0700

    add src/models/users

---
+++ src/models/Users.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const data_schema = mongoose.Schema({
+  username: String,
+  google_id: String,
+  email: String,
+  data_complete: Boolean,
+  data: {
+    gender: String,
+    phone: String,
+    address: String,
+    birth: String,
+  }
+})
+
+module.exports = mongoose.model('Users', data_schema)
