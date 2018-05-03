ca844070d73c656a23fd3569c38e1344257d08ef
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-05-03 17:50:31 +0700

    Add doctor routes

--- src/routes/doctor.js
+++ src/routes/doctor.js
@@ -1,24 +1,47 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const Doctor = require('../models/Doctors');
+const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('get all clinic')
-})
+router.get('/', (req,res) => {
+  Doctor.find({}).exec(function(error, Doctors){
+    if (error)
+      res.send(error)
+    res.send({data: Doctors })
+  });
+});
 
-router.post('/', (req, res) => {
-  res.send('add a clinic')
-})
+router.get('/:id', (req,res) => {
+  Doctor.findOne({_id:req.params.id}).exec(function(error, Doctors ){
+    if (error)
+      res.send(error)
+    res.send({data: Doctors})
+  });
+});
 
-router.get('/:id', (req, res) => {
-  res.send('get a clinic info')
-})
+router.post('/', (req,res) => {
+  Doctor.create(req.body).then(function(error, Doctors){
+    if(error)
+      res.send(error);
+    res.send({data:Doctors});
+  });
+});
 
-router.put('/:id', (req, res) => {
-  res.send('update a clinic')
-})
+router.put('/:id', (req,res) => {
+  Doctor.findByIdAndUpdate({_id:req.params.id},req.body).then(function(){
+    Doctor.findOne({_id:req.params.id}).then(function(error, Doctors){
+    if(error)
+      res.send(error)
+    res.send({data:Doctors});
+    });
+  });
+});
 
-router.delete('/:id', (req, res) => {
-  res.send('delete a clinic')
-})
+router.delete('/:id', (req,res) => {
+  Doctor.findByIdAndRemove({_id:req.params.id}).then(function(Doctors){
+    if(error)
+      res.send(error)
+    res.send({data:Doctors});
+  });
+});
 
-module.exports = router
+module.exports = router;
