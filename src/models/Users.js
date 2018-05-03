1aab2680eb572f52caf3477988272cd0f967cd13
Dei Gratia R R <rdeigratia95@gmail.com>
2018-05-03 11:27:59 +0700

    modified fix form order

--- src/models/Users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const data_schema = mongoose.Schema({
-  username: String,
-  google_id: String,
-  complete_data: { type: Boolean, default: false },
-})
-
-module.exports = mongoose.model('Users', data_schema)
--- src/routes/Order.js
+++ src/routes/Order.js
@@ -3,25 +3,45 @@ const Order = require('../models/Orders');
 const router = express.Router();
 
 router.get('/', (req,res) => {
-  Order.find({}).exec((error, resources) => {
+  Order.find({}).exec(function(error, Orders){
     if (error)
       res.send(error)
-    res.send({data: resources})
-  })
+    res.send({data: Orders})
+  });
+});
+
+router.get('/:id', (req,res) => {
+  Order.findOne({_id:req.params.id}).exec(function(error, Orders){
+    if (error)
+      res.send(error)
+    res.send({data: Orders})
+  });
 });
 
 router.post('/', (req,res) => {
-  Order.create(req.body).then(function(Orders){
-    res.send(Orders);
+  Order.create(req.body).then(function(error, Orders){
+    if(error)
+      res.send(error);
+    res.send({data:resources});
   });
 });
 
 router.put('/:id', (req,res) => {
-  res.send('')
+  Order.findByIdAndUpdate({_id:req.params.id},req.body).then(function(Orders){
+    Order.findOne({_id:req.params.id}).then(function(error, Orders){
+    if(error)
+      res.send(error)
+    res.send({data:Orders});
+    });
+  });
 });
 
 router.delete('/:id', (req,res) => {
-  res.send()
+  Order.findByIdAndRemove({_id:req.params.id}).then(function(Orders){
+    if(error)
+      res.send(error)
+    res.send({data:Orders});
+  });
 });
 
 module.exports = router;
