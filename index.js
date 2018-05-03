3710ca978e85554b0569590ac507ba51ad7c3694
M Haidar Hanif <git@mhaidarhanif.com>
2018-05-03 17:49:40 +0700

    Use plural filenaming and routes

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.use(express.json());
 
 app.use("/", require("./src/routes/index"));
 app.use("/auth", require("./src/routes/auth"));
-app.use("/clinic", require("./src/routes/clinic"));
+app.use("/clinics", require("./src/routes/clinics"));
 app.use("/orders", require("./src/routes/orders"));
-app.use("/doctors", require("./src/routes/doctor"));
+app.use("/doctors", require("./src/routes/doctors"));
 
 app.listen(process.env.PORT || 8000, () =>
   console.log("listening on port 8000")
