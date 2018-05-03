5583898ad9b6d15b337e509368da753e3bf4ff1b
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-05-03 11:47:16 +0700

    fix clinic

--- src/models/Clinic.js
+++ src/models/Clinic.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const data_schema = Schema({
-  company: String,
+  title: String,
   description: String,
   photo: String,
   phone: String,
   address: String,
-  services: Array,
   postal_code: Number,
   doctors: [{ type: Schema.Types.ObjectId, ref: 'Doctors' }]
 })
