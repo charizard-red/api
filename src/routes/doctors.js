5484e6be8b84113d0ca66d12690279d10b5c6669
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-05-06 22:43:31 +0700

    Add file upload for doctors

--- src/routes/doctors.js
+++ src/routes/doctors.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Doctor = require('../models/Doctors');
 const Clinic = require('../models/Clinic');
+const fs = require('fs');
 const router = express.Router();
 
 router.get('/', (req,res) => {
@@ -18,14 +19,32 @@ router.get('/:id', (req,res) => {
 });
 
 router.post('/', (req,res) => {
-  Doctor.create(req.body).then(function(data, error){
-    if(error) return res.send(error);
-    Clinic.findByIdAndUpdate({ _id: req.body.clinic }, {
-      $push: { doctors: data._id }
-    }).then(data_clinic => {
-      res.send({ data: data });
+  function getName(mime){
+    if(mime == 'image/png') return '.png'
+    if(mime == 'image/jpeg') return '.jpg'
+  }
+  let image_id = uniqid("doctor-")
+  let filename = image_id + getName(req.files.icon.mimetype)
+  new Clinic({
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    address: req.body.address,
+    specialist: req.body.specialist,
+    clinic: req.body.clinic
+  }).save()
+  .then(data => {
+    fs.writeFile('public/img/' + filename, req.files.icon.data, (err) => {
+      if (err) return res.send({ text: 'error', msg: err })
+      Clinic.findByIdAndUpdate({ _id: req.body.clinic }, {
+        $push: { doctors: data._id }
+      }).then(data_clinic => {
+        res.send({ data: data });
+      }).catch(err => res.send({ text: 'error', msg: err }))
     })
-  });
+  })
+  .catch(err => res.send({ text: "error", msg: err }))
+  // ---------------------------------------------------------------------------
 });
 
 router.put('/:id', (req,res) => {
