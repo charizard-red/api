ef2b18c78da9001faa3402051c6828f14f38fb4e
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-05-10 13:10:49 +0700

    fixed

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -19,6 +19,14 @@ router.get('/:id', (req,res) => {
   });
 });
 
+router.get('/user/:id', (req,res) => {
+  Order.find({'user_id._id': req.params.id})
+  .exec(function(error, data){
+    if (error) return res.send(error)
+    res.send({data: data})
+  });
+});
+
 router.post('/', jwt_token, (req,res) => {
   if(req.auth.data_complete==false) return res.send({ test: 'error', msg: 'User data is not complete' })
   Order.create({
